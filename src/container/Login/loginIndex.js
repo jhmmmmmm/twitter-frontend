fffec4698a70accf0f73490bf1794ca8d3cf011b
Haomiao <haomiaojasonji@gmail.com>
2023-10-17 03:13:53 -0400

    feat: 🎸 add eslint config

--- src/container/Login/loginIndex.js
+++ src/container/Login/loginIndex.js
@@ -1,39 +1,44 @@
 import './loginIndex.css';
-import { Button, Input, Form, Dialog } from 'antd-mobile';
+import {
+  Button, Input, Form, Dialog,
+} from 'antd-mobile';
+
 const initialValues = {
   username: 'hahaha',
-  password: 'lalal'
+  password: 'lalal',
 };
 
 const Login = () => {
-  const [form] = Form.useForm()
+  const [form] = Form.useForm();
   const onSubmit = () => {
-    const values = form.getFieldsValue()
+    const values = form.getFieldsValue();
     Dialog.alert({
       content: <pre>{JSON.stringify(values, null, 2)}</pre>,
-    })
-  }
+    });
+  };
 
   return (
     <div className="login">
-      <Form 
+      <Form
         form={form}
-        layout='horizontal' mode='card' initialValues={initialValues} 
-        footer={
-          <Button block color='primary' onClick={onSubmit} size='large'>
+        layout="horizontal"
+        mode="card"
+        initialValues={initialValues}
+        footer={(
+          <Button block color="primary" onClick={onSubmit} size="large">
             Login
           </Button>
-        }
+        )}
       >
-        <Form.Item label='username' name='username'>
-          <Input placeholder='please enter username' clearable />
+        <Form.Item label="username" name="username">
+          <Input placeholder="please enter username" clearable />
         </Form.Item>
-        <Form.Item label='passowrd' name='password'>
-          <Input placeholder='please enter password' clearable type='password' />
+        <Form.Item label="passowrd" name="password">
+          <Input placeholder="please enter password" clearable type="password" />
         </Form.Item>
       </Form>
     </div>
   );
-}
+};
 
 export default Login;
