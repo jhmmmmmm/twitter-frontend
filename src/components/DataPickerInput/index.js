8dcf4cc47552c744c16cc9539e99a2ddc3ad9a14
Haomiao <haomiaojasonji@gmail.com>
2023-10-19 03:49:23 -0400

    feat: 🎸 add self-defined birthday selector

--- src/components/DataPickerInput/index.js
+++ src/components/DataPickerInput/index.js
@@ -1,8 +1,44 @@
+import { DatePicker } from 'antd-mobile';
+import { useState } from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
 import style from './index.module.css';
+import datepickerIcon from '../../assets/date.svg';
 
-export default () => (
-  <div className={style.birthdayInput}>
-    <div className={style.birthdayTitle}>Birthday</div>
-    <div className={style.birthdayPlaceholder}>Year/Month/Day</div>
-  </div>
-);
+const DatePickerInput = ({
+  value,
+  onChange,
+}) => {
+  const [visible, setVisible] = useState(false);
+  const onClickDatePicker = () => {
+    setVisible(true);
+  };
+  return (
+    <div>
+      <DatePicker
+        title="Select time"
+        visible={visible}
+        onClose={() => {
+          setVisible(false);
+        }}
+        onConfirm={(val) => {
+          onChange(val);
+        }}
+      />
+      <div className={style.birthdayInput} onClick={onClickDatePicker}>
+        <div className={style.birthdayTitle}>Birthday</div>
+        <div>
+          <span className={style.birthdayPlaceholder}>{value ? moment(value).format('YYYY/MM/DD') : 'Year/Month/Day'}</span>
+          <img className={style.datepickerIcon} src={datepickerIcon} alt="datepickerIcon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+DatePickerInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default DatePickerInput;
--- src/container/Register/registerIndex.js
+++ src/container/Register/registerIndex.js
@@ -1,23 +1,62 @@
 /* eslint-disable max-len */
-import { Input, Button } from 'antd-mobile';
+import { useState } from 'react';
+import { Input, Button, Form } from 'antd-mobile';
 import Header from '../../components/Header';
 import DataPickerInput from '../../components/DataPickerInput';
 
-import style from './registerIndex.module.css';
+import style from './registerIndex.module.scss';
+
+const ACCOUNT_TYPE = {
+  TEL: 0,
+  EMAIL: 1,
+};
 
 const Register = () => {
-  console.log('>>>');
+  const [formData] = useState({
+    name: '12345',
+    tel: '12345',
+    email: '',
+    birthday: '',
+  });
+
+  const [accountType, setAccountType] = useState(ACCOUNT_TYPE.TEL);
+
+  const onAccountTypeChange = () => {
+    if (accountType === ACCOUNT_TYPE.TEL) {
+      setAccountType(ACCOUNT_TYPE.EMAIL);
+      return;
+    }
+    setAccountType(ACCOUNT_TYPE.TEL);
+  };
+
   return (
     <div>
       <div className={style.form}>
         <Header />
         <div className={style.formTitle}>Create your account</div>
-        <Input placeholder="Name" className={style.input} />
-        <Input placeholder="Phone" className={style.input} />
-        <div className={style.changeTypeButton}>Use email instead</div>
-        <div className={style.birthday}>Date of birth</div>
-        <div>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</div>
-        <DataPickerInput />
+        <Form initialValues={formData} className={style.formContainer}>
+          <Form.Item name="name">
+            <Input placeholder="Name" className={style.input} />
+          </Form.Item>
+          {accountType === ACCOUNT_TYPE.TEL && (
+          <Form.Item name="tel">
+            <Input placeholder="Phone" className={style.input} />
+          </Form.Item>
+          )}
+          {accountType === ACCOUNT_TYPE.EMAIL && (
+          <Form.Item name="tel">
+            <Input placeholder="Email" className={style.input} />
+          </Form.Item>
+          )}
+          <div className={style.changeTypeButton} onClick={onAccountTypeChange}>
+            {accountType === ACCOUNT_TYPE.EMAIL ? 'Use phone number instead' : 'Use email instead'}
+          </div>
+          <div className={style.birthday}>Date of birth</div>
+          <div>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</div>
+          <Form.Item name="birthday">
+            <DataPickerInput />
+          </Form.Item>
+        </Form>
       </div>
       <div className={style.footer}>
         <Button className={style.footerButton}>Next</Button>
